c9fd8ea37a4a9b6f87dbb7cc9331556c458bba5a
Jason Fox <jason.fox@fiware.org>
2019-01-28 12:37:15 +0100

    Run `npm run prettier` to format files  - whitespace changes only.

--- resources/performanceTestCheck.js
+++ resources/performanceTestCheck.js
@@ -3,7 +3,7 @@
  */
 function assertEntriesPerCollection(entriesPerCollection) {
   if (!entriesPerCollection || isNaN(entriesPerCollection)) {
-    return print('Usage: assert(entriesPerCollection);')
+    return print('Usage: assert(entriesPerCollection);');
   } else {
     // Get the available databases
     var databases = db.getMongo().getDBs().databases;
@@ -15,13 +15,24 @@ function assertEntriesPerCollection(entriesPerCollection) {
         var database = db.getSisterDB(databases[i].name);
         var collections = database.getCollectionNames();
         for (var j = 0; j < collections.length; j++) {
-          if (collections[j].indexOf('sth_') === 0 && collections[j].indexOf('aggr') === -1) {
+          if (
+            collections[j].indexOf('sth_') === 0 &&
+            collections[j].indexOf('aggr') === -1
+          ) {
             caseCounter++;
             print('** Checking collection ' + collections[j] + '...');
             var count = database[collections[j]].count();
             var percentage = (count / entriesPerCollection) * 100;
-            print('*** Expected ' + entriesPerCollection + ' and found ' + count + '... ' + percentage + '% ' +
-              ((percentage === 100) ? 'PERFECT' : 'ERROR'));
+            print(
+              '*** Expected ' +
+                entriesPerCollection +
+                ' and found ' +
+                count +
+                '... ' +
+                percentage +
+                '% ' +
+                (percentage === 100 ? 'PERFECT' : 'ERROR')
+            );
             if (percentage !== 100) {
               errorCounter++;
             }
