9f02447de5613ac8c6f19deb6e127b38a0b2d277
mapedraza <hola@miguelangelpedraza.com>
2022-03-22 12:53:51 +0100

    Move E2E files to a separate file

---
+++ test/unit/sthE2E_test.js
@@ -0,0 +1,148 @@
+/*
+ * Copyright 2015 Telefónica Investigación y Desarrollo, S.A.U
+ *
+ * This file is part of the Short Time Historic (STH) component
+ *
+ * STH is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * STH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with STH.
+ * If not, see http://www.gnu.org/licenses/.
+ *
+ * For those usages not covered by the GNU Affero General Public License
+ * please contact with: [german.torodelvalle@telefonica.com]
+ */
+
+/* eslint-disable consistent-return */
+
+const ROOT_PATH = require('app-root-path');
+const sthDatabase = require(ROOT_PATH + '/lib/database/sthDatabase');
+const sthDatabaseNameCodec = require(ROOT_PATH + '/lib/database/model/sthDatabaseNameCodec');
+const sthGetDataHandler = require(ROOT_PATH + '/lib/server/handlers/sthGetDataHandler');
+const sthRemoveDataHandler = require(ROOT_PATH + '/lib/server/handlers/sthRemoveDataHandler');
+const sthTestUtils = require(ROOT_PATH + '/test/unit/sthTestUtils.js');
+const sthDatabaseNaming = require(ROOT_PATH + '/lib/database/model/sthDatabaseNaming');
+const sthConfig = require(ROOT_PATH + '/lib/configuration/sthConfiguration');
+const sthUtils = require(ROOT_PATH + '/lib/utils/sthUtils');
+const sthTestConfig = require(ROOT_PATH + '/test/unit/sthTestConfiguration');
+const expect = require('expect.js');
+const _ = require('lodash');
+const INVALID_HOST = 'localhosttest';
+const request = require('request');
+const sth = require(ROOT_PATH + '/lib/sth');
+
+const DATABASE_NAME = sthDatabaseNaming.getDatabaseName(sthConfig.DEFAULT_SERVICE);
+const DATABASE_CONNECTION_PARAMS = {
+    authentication: sthConfig.DB_AUTHENTICATION,
+    dbURI: sthConfig.DB_URI,
+    replicaSet: sthConfig.REPLICA_SET,
+    database: DATABASE_NAME,
+    poolSize: sthConfig.POOL_SIZE
+};
+
+// FIXME: refactor to move this to sthE2E_test.js or something like that.
+// THe sthDatabase_test.js file should appear without changes in the PR after that
+describe('return 500 test', function() {
+    this.timeout(5000);
+    describe('database connection', function() {
+        let contextResponseNumericWithFixedTimeInstantV1;
+
+        before(function(done) {
+            sthDatabase.connect(
+                DATABASE_CONNECTION_PARAMS,
+                function(err) {
+                    if (err) {
+                        return done(err);
+                    }
+                    // It is needed to start the server for e2e test using HTTP endpoint. Otherwise, this step is not needed
+                    sth.sthServer.startServer(sthConfig.STH_HOST, sthConfig.STH_PORT, function() {
+                        sthDatabase.closeConnection(function(err) {
+                            if (err) {
+                                return done(err);
+                            }
+                            contextResponseNumericWithFixedTimeInstantV1 = require('./contextResponses/V1contextResponseNumericWithFixedTimeInstant');
+                            done();
+                        });
+                    });
+                }
+            );
+        });
+
+        after(function(done) {
+            sth.sthServer.stopServer(function() {
+                done();
+            });
+        });
+
+        it('should return 500 when GET http request (e2e)', function(done) {
+            request(
+                {
+                    uri: sthTestUtils.getURL(
+                        sthTestConfig.API_OPERATION.READ,
+                        {
+                            lastN: 0,
+                            dateFrom:
+                                contextResponseNumericWithFixedTimeInstantV1.contextResponses[0].contextElement
+                                    .attributes[0].metadatas[0].value,
+                            dateTo:
+                                contextResponseNumericWithFixedTimeInstantV1.contextResponses[0].contextElement
+                                    .attributes[0].metadatas[0].value
+                        },
+                        contextResponseNumericWithFixedTimeInstantV1.contextResponses[0].contextElement.attributes[0]
+                            .name
+                    ),
+                    method: 'GET',
+                    headers: {
+                        Accept: 'application/json',
+                        'Content-Type': 'application/json',
+                        'Fiware-Service': sthConfig.DEFAULT_SERVICE,
+                        'Fiware-ServicePath': sthConfig.DEFAULT_SERVICE_PATH
+                    }
+                },
+                function(err, response) {
+                    expect(err).to.equal(null);
+                    expect(response.statusCode).to.equal(500);
+                    // FIXME: it would be great to add payload fields expectations
+                    done(err);
+                }
+            );
+        });
+
+        it('should return 500 when DELETE http request (e2e)', function(done) {
+            request(
+                {
+                    uri: sthTestUtils.getURL(
+                        sthTestConfig.API_OPERATION.DELETE,
+                        {
+                            entityType: 'T',
+                            entityId: 'E',
+                            attrName: 'A'
+                        },
+                        'A' // maybe unneeded in API_OPERATION.DELETE case (see getURL method)
+                    ),
+                    method: 'DELETE',
+                    headers: {
+                        Accept: 'application/json',
+                        'Content-Type': 'application/json',
+                        'Fiware-Service': sthConfig.DEFAULT_SERVICE,
+                        'Fiware-ServicePath': sthConfig.DEFAULT_SERVICE_PATH
+                    }
+                },
+                function(err, response) {
+                    expect(err).to.equal(null);
+                    expect(response.statusCode).to.equal(500);
+                    // FIXME: it would be great to add payload fields expectations
+                    done(err);
+                }
+            );
+        });
+    });
+});
